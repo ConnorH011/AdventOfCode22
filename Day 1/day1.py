75f1b0dd55276090bdd3045040bb1e1207bbdb53
ConnorH011 <connor.humphries1@aol.co.uk>
2022-12-08 20:30:03 +0000

    completed day 1
    Day 1, task 1 and 2 completed

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,2 +1,42 @@
 f = open("input.txt", "r")
-print(f.read())
+#print(f.read())
+arr = []
+count = 0
+calCount = 0
+msg = ""
+
+for item in f:
+    count = count + 1
+    #print("line: " + str(count))
+    if item == '\n':
+        msg = ""
+        msg = msg + str(calCount)
+        arr.append(calCount)
+        calCount = 0
+        #print("reset: Final count = " + msg)
+
+    else:
+        calCount = calCount + int(item)
+        #print(calCount)
+
+firstElf = 0
+secondElf = 0
+thirdElf = 0
+
+for i in range (0, len(arr)):
+
+    if arr[i] > firstElf:
+        secondElf = firstElf
+        thirdElf = secondElf
+        firstElf = arr[i]
+    elif arr[i] > secondElf:
+        thirdElf = secondElf
+        secondElf = arr[i]
+    elif arr[i] > thirdElf:
+        thirdElf= arr[i]
+
+max = firstElf + secondElf + thirdElf
+print(thirdElf)
+print(secondElf)
+print(firstElf)
+print("total of all 3 = " + str(max))
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # AdventOfCode22
 Repository to track my progress through the advent of code 2022 challenge 
+This is my first time using python so using it as a way to practise my skills.
 
 Advent of code a series of challenges realsed every day throughout the month of december to test your programming skills.
 Consists of 2 puzzels a day, which, you need to complete the first puzzle to unlock the second. 
